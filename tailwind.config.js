13d9d95920a280bdd9eb0102c67139f976826191
Pawel <pawel.marcinowski11@gmail.com>
2023-09-11 19:40:26 +0200

    improve mobile device ux

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       screens: {
         'xs': '480px',
+        'md': '640px',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addVariant }) {
+      addVariant('hoverable', '@media(hover: hover) and (pointer: fine)')
+    })
+  ]
 }
